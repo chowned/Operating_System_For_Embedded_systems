43bdbb8d67db912652f4b4c9dd0eeb3cc9941a78
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2022-11-14 19:11:31 +0100

    Update lab01_ex02.cpp

--- Lab02/Ex_01/lab01_ex02.cpp
+++ Lab02/Ex_01/lab01_ex02.cpp
@@ -13,17 +13,16 @@ void setup()
 
 TASK(TaskADC)
 {
-    *value = map(analogRead(PIN_INPUT), 0, 1023, 0, 50);        // Bucketization of value between 0 and 50 (resolution 0.1V)
-    SendMessage(msgDataSend, value);                            // send the data to the other task
+    *value = map(analogRead(PIN_INPUT), 0, 1023, 0, 50);        // Bucketization of value between 0 (0V) and 50 (5V) -> (resolution 0.1V)
+    SendMessage(msgDataSend, value);                            // send the data to the other task, send the pointer
     TerminateTask();
 }
 TASK(TaskPWM) 
 {
-    static unsigned short int dataHere;                                          // static, as it must be persistent
+    static unsigned short int dataHere;                                          // static, as it must be persistent among different calls
     if (activationTimes == 0)                                                    // we arrived ad the end of one period
     { ReceiveMessage(msgDataReceiveUnqueued, &dataHere); activationTimes=50; }   // receive the bucket value and restore period
     if(activationTimes --< dataHere){ digitalWrite(PIN_OUTPUT,HIGH); }           // DUTY CYCLE HIGH
     else { digitalWrite(PIN_OUTPUT,LOW); }                                       // DUTY CYCLE LOW
     TerminateTask();
 }
-
