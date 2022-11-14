e2d1a65871daac78c6fef3f9160d25173af8773b
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2022-11-14 12:16:25 +0100

    Removed Bug for correct visualization
    Created pointer to reduce latency.
    Deleted bug that destroyed final resolution.

--- Lab02/Ex_01/lab01_ex02_V1.cpp
+++ Lab02/Ex_01/lab01_ex02_V1.cpp
@@ -1,20 +1,16 @@
 #include "tpl_os.h"
 #include "Arduino.h"
-#include <stdlib.h>
 #define PIN_INPUT A0
 #define PIN_OUTPUT 12
 unsigned short int activationTimes=0;
-unsigned short int* value;
 void setup()
-{
-	pinMode(PIN_OUTPUT, OUTPUT);
-    value = (unsigned short int*) malloc(16); // paid the price only 1 time
-}
+{ pinMode(PIN_OUTPUT, OUTPUT); } //Must declare pin as Output
 
 TASK(TaskADC)
 {
-    *value = map(analogRead(PIN_INPUT), 0, 1023, 0, 50);        // Bucketization of value between 0 and 50 (resolution 0.1V)
-    SendMessage(msgDataSend, value);                            // send the data to the other task
+    unsigned short int value = analogRead(PIN_INPUT);               //Read the analog input
+    value = map(value, 0, 1023, 0, 50);                             // Bucketization of value between 0 and 50 (resolution 0.1V)
+    SendMessage(msgDataSend, &value);                               // send the data to the other task
     TerminateTask();
 }
 TASK(TaskPWM) 
@@ -22,8 +18,7 @@ TASK(TaskPWM)
     static unsigned short int dataHere;                                          // static, as it must be persistent
     if (activationTimes == 0)                                                    // we arrived ad the end of one period
     { ReceiveMessage(msgDataReceiveUnqueued, &dataHere); activationTimes=50; }   // receive the bucket value and restore period
-    if(activationTimes --< dataHere){ digitalWrite(PIN_OUTPUT,HIGH); }           // DUTY CYCLE HIGH
+    if(dataHere >-- activationTimes){ digitalWrite(PIN_OUTPUT,HIGH); }           // DUTY CYCLE HIGH
     else { digitalWrite(PIN_OUTPUT,LOW); }                                       // DUTY CYCLE LOW
     TerminateTask();
 }
-
