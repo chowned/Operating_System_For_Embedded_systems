aceb9fcfe890e35479775df2559153d2f0226eb3
Gaetano Salvatore Falco <gaetanosalvatore.falco@protonmail.com>
2022-11-17 19:51:23 +0100

    Improved latency
    Modified the code to send less data, in order to improve latency. Now it is around 400ms.

--- Lab02/Ex_01/lab02_ex01.cpp
+++ Lab02/Ex_01/lab02_ex01.cpp
@@ -4,16 +4,16 @@
 #define PIN_INPUT A0
 #define PIN_OUTPUT 12
 unsigned short int activationTimes=0;
-unsigned short int* value;
+unsigned char* value;
 void setup()
 {
 	pinMode(PIN_OUTPUT, OUTPUT);
-    value = (unsigned short int*) malloc(16); // paid the price only 1 time
+    value = (unsigned char*) malloc(8); // paid the price only 1 time
 }
 
 TASK(TaskADC)
 {
-    *value = map(analogRead(PIN_INPUT), 0, 1023, 0, 50);                          // Bucketization of value between 0 (0V) and 50 (5V) -> (resolution 0.1V)
+    *value = map(analogRead(PIN_INPUT), 0, 1023, 0, 50);                   // Bucketization of value between 0 (0V) and 50 (5V) -> (resolution 0.1V)
     SendMessage(msgDataSend, value);                                              // send the data to the other task, send the pointer
     TerminateTask();
 }
@@ -22,7 +22,7 @@ TASK(TaskPWM)
     static unsigned short int dataHere;                                           // static, as it must be persistent among different calls
     if (activationTimes == 0)                                                     // we arrived ad the end of one period
     { ReceiveMessage(msgDataReceiveUnqueued, &dataHere); activationTimes=50; }    // receive the bucket value and restore period
-    if(activationTimes ++< dataHere){ digitalWrite(PIN_OUTPUT,HIGH); }            // DUTY CYCLE HIGH
+    if(activationTimes --< dataHere){ digitalWrite(PIN_OUTPUT,HIGH); }            // DUTY CYCLE HIGH
     else { digitalWrite(PIN_OUTPUT,LOW); }                                        // DUTY CYCLE LOW
     TerminateTask();
 }
